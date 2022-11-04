46b31bbaf972ddc0785c728fd5728caa98469d27
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-11-03 23:58:11 -0700

    Server opening error

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
 const { authMiddleware } = require("./utils/auth")
+const db = require('./config/connection')
 
 const { typeDefs, resolvers } = require('./schemas');  
 // typeDefs resolvers in ./schema
-const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -34,7 +35,7 @@ const startApolloServer = async(typeDefs, resolvers) => {
 
     db.once('open', () => {
         console.log(`API server running on port ${PORT}`)
-        console.log(`Use GRAPHQL at http://localhost:${PORT}${server.graphqlPath}`)
+        console.log(`Use GRAPHQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
 }
 
